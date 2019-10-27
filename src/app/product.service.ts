bf4b2a4607365eb0dc615f8794f3fdbc480151a9
Joshua Oke <joshua.oke@dipolediamond.com>
2019-10-27 22:42:41 +0100

    still on module 3, further changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+// imports data from this map which is a json file
 
 @Injectable()
 export class ProductService {
-
-  private _albumUrl: '../assets/album.json';
+ // set a url for the album
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) {
 
    }
-
+   getAlbum(id: number) {
+     return this._http.get(this._albumUrl).map((response) => {
+       response.json()
+     });
+   }
 }
